4ef4b8a78604a79fdd9dad5e04b107f7b7d03bdb
Qais Patankar <qaisjp@gmail.com>
2017-11-16 20:22:27 +0000

    Add simple Displacement tests

--- src/tourguide/DisplacementTest.java
+++ src/tourguide/DisplacementTest.java
@@ -3,8 +3,7 @@
  */
 package tourguide;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
@@ -22,5 +21,97 @@ public class DisplacementTest {
         double bearing = new Displacement(0.0, 1.0).bearing();
         assertEquals(0.0, bearing, EPS);
     }
+
+    @Test
+    public void testEastBearing() {
+        double bearing = new Displacement(1.0, 0.0).bearing();
+        assertEquals(90.0, bearing, EPS);
+    }
+
+    @Test
+    public void testSouthBearing() {
+        double bearing = new Displacement(0.0, -1.0).bearing();
+        assertEquals(180, bearing, EPS);
+    }
+
+    @Test
+    public void testWestBearing() {
+        double bearing = new Displacement(-1.0, 0.0).bearing();
+        assertEquals(270, bearing, EPS);
+    }
+
+    @Test
+    public void testNorthWestBearing() {
+        double bearing = new Displacement(-1.0, 1.0).bearing();
+        assertEquals(315, bearing, EPS);
+    }
+
+    @Test
+    public void testNorthEastBearing() {
+        double bearing = new Displacement(1.0, 1.0).bearing();
+        assertEquals(45, bearing, EPS);
+    }
+
+    @Test
+    public void testSouthEastBearing() {
+        double bearing = new Displacement(1.0, -1.0).bearing();
+        assertEquals(135, bearing, EPS);
+    }
+
+    @Test
+    public void testSouthWestBearing() {
+        double bearing = new Displacement(-1.0, -1.0).bearing();
+        assertEquals(225, bearing, EPS);
+    }
+
+
+    @Test
+    public void testNorthDistance() {
+        double distance = new Displacement(0.0, 1.0).distance();
+        assertEquals(1, distance, EPS);
+    }
+
+    @Test
+    public void testEastDistance() {
+        double distance = new Displacement(1.0, 0.0).distance();
+        assertEquals(1, distance, EPS);
+    }
+
+    @Test
+    public void testSouthDistance() {
+        double distance = new Displacement(0.0, -1.0).distance();
+        assertEquals(1, distance, EPS);
+    }
+
+    @Test
+    public void testWestDistance() {
+        double distance = new Displacement(-1.0, 0.0).distance();
+        assertEquals(1, distance, EPS);
+    }
+
+    @Test
+    public void testNorthWestDistance() {
+        double distance = new Displacement(-1.0, 1.0).distance();
+        assertEquals(Math.sqrt(2), distance, EPS);
+    }
+
+    @Test
+    public void testNorthEastDistance() {
+        double distance = new Displacement(1.0, 1.0).distance();
+        assertEquals(Math.sqrt(2), distance, EPS);
+    }
+
+    @Test
+    public void testSouthEastDistance() {
+        double distance = new Displacement(1.0, -1.0).distance();
+        assertEquals(Math.sqrt(2), distance, EPS);
+    }
+
+    @Test
+    public void testSouthWestDistance() {
+        double distance = new Displacement(-1.0, -1.0).distance();
+        assertEquals(Math.sqrt(2), distance, EPS);
+    }
+
 
 }
