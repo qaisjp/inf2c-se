9b993c57ad5a23efafaa86abc17e743baa546e62
Qais Patankar <qaisjp@gmail.com>
2017-11-16 18:59:59 +0000

    Initial JavaDoc code for Displacement

--- src/tourguide/Displacement.java
+++ src/tourguide/Displacement.java
@@ -2,12 +2,24 @@ package tourguide;
 
 import java.util.logging.Logger;
 
+/**
+ *
+ */
 public class Displacement {
     private static Logger logger = Logger.getLogger("tourguide");
 
+    // x-value
     public double east;
+
+    // y-value
     public double north;
 
+    /**
+     * Class constructor for the displacement.
+     *
+     * @param e the offset towards the east
+     * @param n the offset towards the north
+     */
     public Displacement(double e, double n) {
         logger.finer("East: " + e + "  North: " + n);
 
@@ -15,13 +27,22 @@ public class Displacement {
         north = n;
     }
 
+    /**
+     * Find the distance of the displacement from the origin.
+     *
+     * @return the distance from the origin
+     */
     public double distance() {
         logger.finer("Entering");
 
         return Math.sqrt(east * east + north * north);
     }
 
-    // Bearings measured clockwise from north direction.
+    /**
+     * Get the bearing of the displacement.
+     *
+     * @return the bearing measured clockwise form the north direction
+     */
     public double bearing() {
         logger.finer("Entering");
 
