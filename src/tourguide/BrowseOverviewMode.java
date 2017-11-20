477d2a11efe1ed2e6bc8c61d0038910b8cffc530
Qais Patankar <qaisjp@gmail.com>
2017-11-20 23:41:03 +0000

    Initial BrowseOverview implementation

--- src/tourguide/BrowseOverviewMode.java
+++ src/tourguide/BrowseOverviewMode.java
@@ -2,10 +2,24 @@ package tourguide;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class BrowseOverviewMode extends Mode {
+    ArrayList<Chunk> output = new ArrayList<>();
+
     public BrowseOverviewMode() {
         super(ModeType.BROWSE_OVERVIEW);
+
+        output.add(new Chunk.BrowseOverview());
+    }
+
+    public BrowseOverviewMode(Set<Tour> tours) {
+        this();
+
+        Chunk.BrowseOverview overview = (Chunk.BrowseOverview) output.get(0);
+        for (Tour t : tours) {
+            overview.addIdAndTitle(t.getID(), t.getTitle());
+        }
     }
 
     @Override
