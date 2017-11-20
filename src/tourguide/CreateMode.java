29d7c31bc41e6ab1180f676344b76ebf22493873
Qais Patankar <qaisjp@gmail.com>
2017-11-20 23:40:33 +0000

    Initial Create implementation

--- src/tourguide/CreateMode.java
+++ src/tourguide/CreateMode.java
@@ -1,14 +1,31 @@
 package tourguide;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreateMode extends Mode {
-    public CreateMode() {
+    private Tour tour;
+
+    public CreateMode(Tour tour) {
         super(ModeType.CREATE);
+
+        this.tour = tour;
     }
 
     @Override
     List<Chunk> getOutput() {
-        return null;
+        ArrayList<Chunk> chunks = new ArrayList<>();
+
+        chunks.add(new Chunk.CreateHeader(
+                tour.getTitle(),
+                tour.getLegs(),
+                tour.getWaypoints()
+        ));
+
+        return chunks;
+    }
+
+    public Tour getTour() {
+        return tour;
     }
 }
