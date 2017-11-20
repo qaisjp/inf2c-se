742dca5f6ae4e0fe6b255baf57054083c839f27a
Qais Patankar <qaisjp@gmail.com>
2017-11-20 23:39:22 +0000

    Expand Tour with more getters

--- src/tourguide/Tour.java
+++ src/tourguide/Tour.java
@@ -1,9 +1,14 @@
 package tourguide;
 
+import java.util.ArrayList;
+
 public class Tour {
     private String id;
     private String title;
     private Annotation annotation;
+
+    private ArrayList<Annotation> waypoints;
+    private ArrayList<Annotation> legs;
 
     public Tour(String id, String title, Annotation annotation) {
         this.id = id;
@@ -18,5 +23,21 @@ public class Tour {
     @Override
     public int hashCode() {
         return id.hashCode();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLegs() {
+        return legs.size();
+    }
+
+    public int getWaypoints() {
+        return waypoints.size();
+    }
+
+    public String getID() {
+        return id;
     }
 }
