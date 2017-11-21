571fdf2ae18dbb6c92652ede3a985e4909ceaaaa
Qais Patankar <qaisjp@gmail.com>
2017-11-21 03:54:01 +0000

    Add many log messages to FollowMode

--- src/tourguide/FollowMode.java
+++ src/tourguide/FollowMode.java
@@ -2,15 +2,19 @@ package tourguide;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class FollowMode extends Mode {
+    private static Logger logger = Logger.getLogger("tourguide");
+
     private Tour tour;
-    private int stage;
+    private int stage = 0;
     private Displacement location;
     private double waypointRadius;
 
     public FollowMode(Tour tour, double waypointRadius) {
         super(ModeType.FOLLOW);
+        logger.finer("Tour: " + tour.getID() + ",  waypointRadius: " + waypointRadius);
 
         this.tour = tour;
         this.waypointRadius = waypointRadius;
@@ -23,15 +27,17 @@ public class FollowMode extends Mode {
      * @param location the user is currently at
      * @return completion state
      */
-    public boolean setLocation(Displacement location) {
-        this.location = location;
+    public boolean setLocation(Displacement loc) {
+        logger.finer("Entering with location(e: " + loc.east + ", n: " + loc.north + ")");
+        this.location = loc;
 
         Displacement d = displacementToNext();
         if (d != null && d.distance() <= waypointRadius) {
             stage += 1;
-
+            logger.fine("stage advanced with displacement(e: " + d.east + ", n: " + d.north + ")");
         }
 
+        logger.finest("is current stage final: " + String.valueOf(currentStage().isFinal()));
         return currentStage().isFinal();
     }
 
@@ -42,6 +48,12 @@ public class FollowMode extends Mode {
     private Displacement displacementToNext() {
         Stage nextStage = tour.getStages().get(stage + 1);
         if (nextStage == null || nextStage.waypoint == null) {
+            logger.finer((nextStage == null) ? "nextStage null" : "nextStage.waypoint null");
+
+            if (nextStage.waypoint == null) {
+                logger.finest("stage: " + stage + ", nextStage: " + nextStage.toString());
+            }
+
             return null;
         }
 
@@ -53,6 +65,8 @@ public class FollowMode extends Mode {
 
     @Override
     List<Chunk> getOutput() {
+        logger.finer("Entering");
+
         ArrayList<Chunk> chunks = new ArrayList<>();
         chunks.add(new Chunk.FollowHeader(
             tour.getTitle(),
@@ -62,6 +76,7 @@ public class FollowMode extends Mode {
 
         Waypoint waypoint = currentStage().waypoint;
         if (waypoint != null) {
+            logger.finest("Adding Chunk.FollowWaypoint: " + waypoint.getAnnotation());
             chunks.add(new Chunk.FollowWaypoint(
                     waypoint.getAnnotation()
             ));
@@ -69,6 +84,7 @@ public class FollowMode extends Mode {
 
         Leg leg = currentStage().leg;
         if (leg != null) {
+            logger.finest("Adding Chunk.FollowLeg: " + leg.getAnnotation());
             chunks.add(new Chunk.FollowLeg(
                     leg.getAnnotation()
             ));
@@ -77,6 +93,7 @@ public class FollowMode extends Mode {
         // What happens if we do this when we have finished?
         Displacement d = displacementToNext();
         if (d != null) {
+            logger.finest("Adding Chunk.FollowBearing: bearing(" + d.bearing()+ "), distance(" + d.distance() + ")");
             chunks.add(new Chunk.FollowBearing(
                     d.bearing(), d.distance()
             ));
