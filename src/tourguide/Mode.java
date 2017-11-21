042b696e487a477348d36d673c1cdda494d27c67
Qais Patankar <qaisjp@gmail.com>
2017-11-21 19:53:13 +0000

    Move ModeType into Mode class, as subclass

--- src/tourguide/Mode.java
+++ src/tourguide/Mode.java
@@ -3,6 +3,20 @@ package tourguide;
 import java.util.List;
 
 public abstract class Mode {
+
+    private enum ModeType {
+        CREATE,
+        BROWSE_OVERVIEW,
+        BROWSE_DETAILS,
+        FOLLOW;
+
+        boolean IsBrowse() {
+            return (this == BROWSE_DETAILS) ||  (this == BROWSE_OVERVIEW);
+        }
+        boolean IsCreate() { return this == CREATE; }
+        boolean IsFollow() { return this == FOLLOW; }
+    }
+
     private final ModeType type;
 
     public Mode(ModeType type) {
